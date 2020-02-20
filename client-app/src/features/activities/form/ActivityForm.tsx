eb6e07c8781ed55f82e3b5943458dd39f061deef
besnault <berangere.esnault@orange.fr>
2020-02-20 16:17:25 +0100

    add styles

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import ActivityStore from "../../../app/stores/ActivityStore";
@@ -25,26 +25,32 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   } = activityStore;
 
   const [activity, setActivity] = useState<IActivity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: '',
-    city: '',
-    venue: ''
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: ""
   });
 
-    // ajouter un tableau vide au minimum en second paramètre sinon boucle infinie
-    useEffect(() => {
-      if (match.params.id && activity.id.length === 0) {
-        loadActivity(match.params.id).then(
-          () => initialFormState && setActivity(initialFormState)
-        );
-      }
-      return (() => {
-        clearActivity();
-      })
-    }, [loadActivity, match.params.id, clearActivity, initialFormState, activity.id.length]);
+  // ajouter un tableau vide au minimum en second paramètre sinon boucle infinie
+  useEffect(() => {
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
+    }
+    return () => {
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    match.params.id,
+    clearActivity,
+    initialFormState,
+    activity.id.length
+  ]);
 
   const handleSubmit = () => {
     if (activity.id.length === 0) {
@@ -52,9 +58,13 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
         ...activity,
         id: uuid()
       };
-      createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+      createActivity(newActivity).then(() =>
+        history.push(`/activities/${newActivity.id}`)
+      );
     } else {
-      editActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+      editActivity(activity).then(() =>
+        history.push(`/activities/${activity.id}`)
+      );
     }
   };
 
@@ -67,61 +77,65 @@ export const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
 
   return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit}>
-        <Form.Input
-          onChange={handleInputChange}
-          name="title"
-          placeholder="Titre"
-          value={activity.title}
-        />
-        <Form.TextArea
-          rows={3}
-          onChange={handleInputChange}
-          name="description"
-          placeholder="Description"
-          value={activity.description}
-        />
-        <Form.Input
-          onChange={handleInputChange}
-          name="category"
-          placeholder="Catégorie"
-          value={activity.category}
-        />
-        <Form.Input
-          type="datetime-local"
-          onChange={handleInputChange}
-          name="date"
-          placeholder="Date"
-          value={activity.date}
-        />
-        <Form.Input
-          placeholder="Ville"
-          onChange={handleInputChange}
-          name="city"
-          value={activity.city}
-        />
-        <Form.Input
-          placeholder="Destination"
-          onChange={handleInputChange}
-          name="venue"
-          value={activity.venue}
-        />
-        <Button
-          loading={submitting}
-          floated="right"
-          positive
-          type="submit"
-          content="Publier"
-        />
-        <Button
-          onClick={() => history.push('/activities')}
-          floated="right"
-          type="button"
-          content="Annuler"
-        />
-      </Form>
-    </Segment>
+    <Grid>
+      <Grid.Column>
+        <Segment clearing>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input
+              onChange={handleInputChange}
+              name="title"
+              placeholder="Titre"
+              value={activity.title}
+            />
+            <Form.TextArea
+              rows={3}
+              onChange={handleInputChange}
+              name="description"
+              placeholder="Description"
+              value={activity.description}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="category"
+              placeholder="Catégorie"
+              value={activity.category}
+            />
+            <Form.Input
+              type="datetime-local"
+              onChange={handleInputChange}
+              name="date"
+              placeholder="Date"
+              value={activity.date}
+            />
+            <Form.Input
+              placeholder="Ville"
+              onChange={handleInputChange}
+              name="city"
+              value={activity.city}
+            />
+            <Form.Input
+              placeholder="Destination"
+              onChange={handleInputChange}
+              name="venue"
+              value={activity.venue}
+            />
+            <Button
+              loading={submitting}
+              floated="right"
+              positive
+              type="submit"
+              content="Publier"
+            />
+            <Button
+              onClick={() => history.push("/activities")}
+              floated="right"
+              type="button"
+              content="Annuler"
+            />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
