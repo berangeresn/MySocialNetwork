227ec5cfb4d7a2ddcb642dbaf2eca97308f7ba59
besnault <berangere.esnault@orange.fr>
2020-02-27 14:05:07 +0100

    add, delete, setmain photo features

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -11,7 +12,8 @@ namespace Application.Activities
             CreateMap<UserActivity, AttendeeDto>()
             // d pour destination et s pour source
             .ForMember(d => d.Username, obj => obj.MapFrom(s => s.AppUser.UserName))
-            .ForMember(d => d.DisplayName, obj => obj.MapFrom(s => s.AppUser.DisplayName));
+            .ForMember(d => d.DisplayName, obj => obj.MapFrom(s => s.AppUser.DisplayName))
+            .ForMember(d => d.Image, obj => obj.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
